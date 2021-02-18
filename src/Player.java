c55871cab2597d36bcbcf3558eca3705e350ceab
Luke Hanna <Luke.m.hanna@gmail.com>
2021-02-18 12:29:00 +0400

    Added player class to manage paddle information

---
+++ src/Player.java
@@ -0,0 +1,26 @@
+/**
+ * Simple player class that gets passed around from server to client
+ *
+ * Bugs: None known
+ * @author Luke Hanna (Github.com/JustAPyro)
+ * @date Feb 18, 2021
+ * @version 1.0
+ */
+public class Player
+{
+
+    boolean left; // left or right player
+    int height; // Height of paddle
+
+    /**
+     * Default constructor assigns a paddle to left or right and sets the height.
+     *
+     * @param pos can be "left" or "right" depending on which side the players paddle is on
+     * @param height the vertical position of the paddle
+     */
+    public Player(String pos, int height)
+    {
+
+    }
+
+}
